6e3faeed37c3a9e86d5d484b4d037df525aa29fa
samkeen <samkeen@lululemon.com>
2020-07-26 16:21:17 -0700

    init commit

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] private Transform player;
+
+    private NavMeshAgent _navMeshAgent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NavigateToPlayer();
+    }
+
+    private void NavigateToPlayer()
+    {
+        _navMeshAgent.SetDestination(player.position);
+    }
+}
