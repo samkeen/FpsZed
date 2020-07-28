0008cd72c3c5339d29202a84596f271f48dd6eb0
samkeen <samkeen@lululemon.com>
2020-07-27 20:47:28 -0700

    added raycaster, e:176

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] private Camera FPCamera;
+    [SerializeField] private float range = 100f;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        var fpTransform = FPCamera.transform;
+        Physics.Raycast(fpTransform.position, fpTransform.forward, out hit, range);
+        Debug.Log($"I hit {hit.transform.name}");
+    }
+}
